1c29a3762f8ffd52e9bfc8918ac1e179e2a39042
Len_AshBell <jason1022liu@gmail.com>
2020-03-13 12:54:49 +0800

    icon & pwa setting & build ver.

--- DietCalendar/src/environments/environment.prod.ts
+++ DietCalendar/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBNQPUEBYzBghUvNYKpYiJMPccXIWfrobo',
+    authDomain: 'fir-78142.firebaseapp.com',
+    databaseURL: 'https://fir-78142.firebaseio.com',
+    projectId: 'fir-78142',
+    storageBucket: 'fir-78142.appspot.com',
+    messagingSenderId: '699440668814',
+    appId: '1:699440668814:web:f3b50a1717f478318be9df'
+  }
 };
